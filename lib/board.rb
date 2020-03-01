defe50bcfdc7ea815e344bf174c842f9d9c66808
Danieleclima <daniel.echuaca.lima@gmail.com>
2020-03-01 18:52:20 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -13,9 +13,29 @@ class Board
   end
   
   def display
-    print self.cells[0..2].join(" | ")
+    puts self.cells[0..2].join(" | ")
+    puts self.cells[3..5].join(" | ")
+    puts self.cells[6..8].join(" | ")
+  end
+
+  def position
   end
   
+  def update (position, player)
+  end
+
+  def full?
+  end
+
+  def turn_count
+  end
+
+  def taken?
+  end
+
+  def valid_move?
+  end
+
 end
 
 #board = Board.new
---
+++ lib/game.rb
@@ -0,0 +1,6 @@
+class Game
+
+    def call
+        puts "Let's start playing"
+    end
+end
