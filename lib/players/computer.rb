d29abbe202995f9ff1fbb3e024e2d4771cd8f9e9
Danieleclima <daniel.echuaca.lima@gmail.com>
2020-03-13 21:47:34 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,7 +2,12 @@ module Players
 class Computer < Player
       
     def move(board)
+        if !board.taken?(5)
+        computer_move = 5
+        else
         computer_move = rand(1..9)
+        end
+
         if board.valid_move?(computer_move)
             computer_move.to_s
         else
